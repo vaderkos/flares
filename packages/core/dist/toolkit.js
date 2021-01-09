60bdf8ab9fedffad3f75fda76f70b703c30f622d
vaderkos <mykyta.smirnov.dev@gmail.com>
2021-01-09 20:14:11 +0100

    dists removed

--- packages/core/dist/toolkit.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=toolkit.js.map
--- packages/express/dist/express-toolkit.d.ts
+++
@@ -1,5 +0,0 @@
-import { ErrorRequestHandler, RequestHandler } from 'express-serve-static-core';
-export declare type AsyncRequestHandler<T> = (...args: Parameters<RequestHandler>) => Promise<T>;
-export declare type AsyncErrorRequestHandler<T> = (...args: Parameters<ErrorRequestHandler>) => Promise<T>;
-export declare function AsyncRequestHandler<T>(handler: AsyncRequestHandler<T>): RequestHandler;
-export declare function AsyncErrorRequestHandler<T>(errorHandler: AsyncErrorRequestHandler<T>): ErrorRequestHandler;
